49a08c1c5f06b4cb9023fded91c6ec6f6e7698de
EasonMo <moyicheng1990@gmail.com>
2024-09-21 18:15:51 +0800

    fix: 修正gnome剪贴板

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,6 +19,11 @@ vim.opt.smartindent = false
 -- vim.opt.conceallevel = 0
 vim.opt.backspace = "eol,start,indent"
 
+-- 修正linux下的剪贴板
+if os.getenv("XDG_CURRENT_DESKTOP") == "GNOME" then
+  vim.opt.clipboard = "unnamedplus"
+end
+
 -----------------------✂---------------------------
 --                LazyVim配置
 -----------------------✂---------------------------
