a5bbc9bb7121a9562d719a46007ba211f00839e5
EasonMo <moyicheng1990@gmail.com>
2024-10-09 01:40:30 +0800

    feat: dap repl补全和高亮

--- lua/plugins/dap/core.lua
+++ lua/plugins/dap/core.lua
@@ -1,3 +1,5 @@
+local utils = require("utils")
+
 ---@param config {args?:string[]|fun():string[]?}
 local function get_args(config)
   local args = type(config.args) == "function" and (config.args() or {}) or config.args or {}
@@ -20,13 +22,13 @@ return {
       { "<leader>dc", function() Close_all();require("dap").continue() end, desc = "Continue" },
       { "<F5>", function() Close_all(); require("dap").continue() end, desc = "DAP: Continue" },
       { "<F9>", function() require("dap").toggle_breakpoint() end, desc = "DAP: Toggle Breakpoint" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "DAP: Step Over" },
       { "<F10>", function() require("dap").step_over() end, desc = "DAP: Step Over" },
       { "<F11>", function() require("dap").step_into() end, desc = "DAP: Step Into" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "DAP: Step Out" },
       { "<S-F11>", function() require("dap").step_out() end, desc = "DAP: Step Out" },
-      { "<F23>", function() require("dap").step_out() end, desc = "DAP: Step Out" },
+      { "<F23>", function() require("dap").step_out() end, desc = "DAP: Step Out" },  -- 即shift+F11
       { "<leader>dx", function() require("dap").clear_breakpoints() end, desc = "Clear Breakpoint"},
-      { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
-      { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
     },
   },
   {
@@ -49,6 +51,45 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close({})
       end
+    end,
+  },
+  -- dap补全
+  {
+    "rcarriga/cmp-dap",
+    event = "VeryLazy",
+    config = function()
+      require("cmp").setup({
+        enabled = function()
+          ---@diagnostic disable-next-line: deprecated
+          return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+        end,
+      })
+
+      require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+        sources = {
+          { name = "dap" },
+        },
+      })
+      local dap = require("dap")
+      -- 修正dap-repl buffer不关闭
+      dap.listeners.before["event_terminated"]["hide-dap-repl"] = function()
+        local bufs = utils.find_buffers_by_filetype("dap-repl")
+        for _, buf in ipairs(bufs) do
+          vim.bo[buf].buflisted = false
+        end
+      end
+    end,
+  },
+  -- dap高亮
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "dap_repl" } },
+  },
+  {
+    "LiadOz/nvim-dap-repl-highlights",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-dap-repl-highlights").setup()
     end,
   },
 }
