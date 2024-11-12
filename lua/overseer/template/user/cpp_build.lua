713500c7663ccb755e4b95886ee78f0cce3d80e7
EasonMo <moyicheng1990@gmail.com>
2024-11-13 06:26:01 +0800

    添加overseer

---
+++ lua/overseer/template/user/cpp_build.lua
@@ -0,0 +1,26 @@
+return {
+  name = "nvim: gcc build active file",
+  builder = function()
+    local file = vim.fn.expand("%:.")
+    local filetype = vim.fn.expand("%:e")
+    local gcc_cmd = "gcc"
+    if filetype == "c" then
+      gcc_cmd = "g++"
+    end
+    local out = vim.fn.expand("%:r") .. ".out"
+    return {
+      cmd = { gcc_cmd },
+      args = { "-g", file, "-o", out },
+      components = {
+        -- { "on_output_quickfix", open = true },
+        { "user.on_output_quickfix_trouble", open = true },
+        -- "on_result_diagnostics",
+        -- { "restart_on_save", paths = { vim.fn.expand("%:p") } },
+        "default",
+      },
+    }
+  end,
+  condition = {
+    filetype = { "c", "cpp" },
+  },
+}
