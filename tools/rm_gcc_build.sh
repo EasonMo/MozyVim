7f6603d191efef261b24ec3411c98a10b47cba9b
EasonMo <moyicheng1990@gmail.com>
2024-12-29 01:37:03 +0800

    feat: overseer添加task.json的component，优化rm_gcc_build.sh

--- tools/rm_gcc_build.sh
+++ tools/rm_gcc_build.sh
@@ -1,3 +1,4 @@
+find . -name "*.out.dSYM" -type d -print -exec rm -rf {} +
 find . -name "*.out" -type f -delete -print
 find . -name "*.fifo" -type f -delete -print
 find . -name "*.o" -type f -delete -print
