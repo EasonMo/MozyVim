98d203883a1e949808bfa93efb67c41c14f2e615
EasonMo <moyicheng1990@gmail.com>
2025-01-03 22:54:06 +0800

    fix: 修复visual模式S添加surround失效

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -104,6 +104,9 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    dependencies = {
+      "folke/flash.nvim",
+    },
     event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({})
