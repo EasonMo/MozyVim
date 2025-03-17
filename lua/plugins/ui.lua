364ee94a664256cc763ea7f50449cc6e2965dc97
EasonMo <moyicheng1990@gmail.com>
2025-03-17 17:43:02 +0800

    feat: 修改在当前目录打开终端和grep

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -310,6 +310,7 @@ return {
   {
     "folke/snacks.nvim",
     keys = {
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
       { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     },
     ---@type snacks.Config
