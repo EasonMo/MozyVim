fd7e5c974d28b1f9fa03232d9f5b625a5c7988c4
EasonMo <moyicheng1990@gmail.com>
2025-07-06 23:03:10 +0800

    fix: lsp接口更新

--- lua/plugins/lsp/js.lua
+++ lua/plugins/lsp/js.lua
@@ -6,7 +6,7 @@ return {
     servers = {
       vtsls = {
         handlers = {
-          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+          ["textDocument/publishDiagnostics"] = function(_, result, ctx)
             if result.diagnostics ~= nil then
               local idx = 1
               while idx <= #result.diagnostics do
@@ -18,7 +18,7 @@ return {
                 end
               end
             end
-            vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+            vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
           end,
         },
       },
