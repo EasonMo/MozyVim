31f05cf70d00d52ce6e9032dd2a5ab6812be9c00
EasonMo <moyicheng1990@gmail.com>
2025-09-01 12:31:49 +0800

    docs: 更新注释

--- lua/plugins/pin.lua
+++ lua/plugins/pin.lua
@@ -1,3 +1,6 @@
 -- 临时pin插件版本，等待插件作者修复
+-- git update-index --assume-unchanged lua/plugins/pin.lua
+-- git update-index --no-assume-unchanged lua/plugins/pin.lua
+-- git ls-files -v | grep '^h\ '
 return {
 }
